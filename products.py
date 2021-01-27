983e093bf5709eea8c1c3c5dc05b4e43c4122fa2
John Doe <j127289j@gmail.com>
2021-01-27 22:46:49 +0800

    import file checking

--- products.py
+++ products.py
@@ -1,15 +1,24 @@
+#載入作業系統函式庫
+import os
+#檢察檔案是否存在
+
+
 products = []
-#讀取檔案
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		#加入continue略過商品價格，不加入到products清單中
-		if '商品,價格' in line:
-			continue
-		#先strip多餘空格與\n換行符號，再以讀取資料內的「,」為分割，並且一次存在兩個變數中
-		name, price = line.strip().split(',')
-		#存入成二維清單
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):
+	print('找到檔案')
+	#讀取檔案
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			#加入continue略過商品價格，不加入到products清單中
+			if '商品,價格' in line:
+				continue
+			#先strip多餘空格與\n換行符號，再以讀取資料內的「,」為分割，並且一次存在兩個變數中
+			name, price = line.strip().split(',')
+			#存入成二維清單
+			products.append([name, price])
+	print(products)
+else:
+	print('沒有找到檔案喔')
 #輸入資料並存入清單
 while True:
 	name = input('請輸入商品名稱: ')
