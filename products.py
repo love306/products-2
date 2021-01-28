e99aaacc2893405b8bd1b5ec965563b0c076173f
John Doe <j127289j@gmail.com>
2021-01-28 22:52:56 +0800

    add function

--- products.py
+++ products.py
@@ -1,13 +1,18 @@
 #載入作業系統函式庫
 import os
-#檢察檔案是否存在
 
+def check_file(filename):
 
-products = []
-if os.path.isfile('products.csv'):
-	print('找到檔案')
+	#檢察檔案是否存在
+	if os.path.isfile(filename):
+			print('找到檔案')
+	else:
+			print('沒有找到檔案喔')
+
+def read_file(filename):
+	products = []	
 	#讀取檔案
-	with open('products.csv', 'r', encoding='utf-8') as f:
+	with open(filename, 'r', encoding='utf-8') as f:
 		for line in f:
 			#加入continue略過商品價格，不加入到products清單中
 			if '商品,價格' in line:
@@ -16,24 +21,60 @@ if os.path.isfile('products.csv'):
 			name, price = line.strip().split(',')
 			#存入成二維清單
 			products.append([name, price])
+	#印出清單內容
 	print(products)
-else:
-	print('沒有找到檔案喔')
-#輸入資料並存入清單
-while True:
-	name = input('請輸入商品名稱: ')
-	if name == 'q':
-		break
-	price = input('請輸入商品價格: ')
-	products.append([name, price])
-print(products)
-#印出每個products的資料
-for p in products:
-	print(p[0], '的價格為', p[1])
-#寫入檔案
-with open('products.csv', 'w', encoding='utf-8') as f:
-	#在第一行加入標籤
-	f.write('商品,價格\n')
+	return products
 
+def user_input(products):	
+	#輸入資料並存入清單
+	while True:
+		name = input('請輸入商品名稱: ')
+		if name == 'q':
+			break
+		price = input('請輸入商品價格: ')
+		products.append([name, price])
+	print(products)
+	return products
+
+def print_products(products):	
+	#印出每個products的資料
 	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+		print(p[0], '的價格為', p[1])
+
+def write_file(filename, products):
+	#寫入檔案
+	with open(filename, 'w', encoding='utf-8') as f:
+		#在第一行加入標籤
+		f.write('商品,價格\n')
+		for p in products:
+			f.write(p[0] + ',' + p[1] + '\n')
+#加入開關
+def on_off(on=True):
+	while True:
+		#自定義開啟檔名
+		filename = input('請輸入需開啟之檔案（含副檔名）: ')
+		if os.path.isfile(filename):
+			print('找到檔案')
+			products = read_file(filename)
+			products = user_input(products)
+		else:
+			print('沒有找到檔案喔')
+			break
+		#決定讀取後動作
+		write_or_print = input('列出清單內容並寫入檔案輸入1，直接寫入檔案輸入2，只列出清單內容輸入3: ')
+		if write_or_print == '1':
+			print_products(products)
+			write_file(filename, products)
+			break
+		elif write_or_print == '2':
+			write_file(filename, products)
+			break
+		elif write_or_print == '3':
+		 	print_products(products)
+		 	break
+on_off()
+
+
+
+
+
